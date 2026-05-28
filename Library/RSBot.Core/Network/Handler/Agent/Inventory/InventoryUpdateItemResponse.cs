06c83418658167061d814b0ac7a1f27a17029428
kis1yi <35934681+kis1yi@users.noreply.github.com>
2026-05-28 23:01:04 +0200

    Updated pets spawn for rusro

--- Library/RSBot.Core/Network/Handler/Agent/Inventory/InventoryUpdateItemResponse.cs
+++ Library/RSBot.Core/Network/Handler/Agent/Inventory/InventoryUpdateItemResponse.cs
@@ -29,13 +29,15 @@ internal class InventoryUpdateItemResponse : IPacketHandler
     /// <param name="packet">The packet.</param>
     public void Invoke(Packet packet)
     {
-        if (Game.ClientType == GameClientType.Global)
+        if (Game.ClientType == GameClientType.Global
+            || Game.ClientType == GameClientType.RuSro)
             if (packet.ReadByte() != 0) //sometimes appears 9 with unknown structure
                 return; 
 
         var sourceSlot = packet.ReadByte();
 
-        if (Game.ClientType == GameClientType.Global)
+        if (Game.ClientType == GameClientType.Global
+            || Game.ClientType == GameClientType.RuSro)
             packet.ReadByte(); //0 - normal, 2 - item disappearing
 
         var itemUpdateFlag = (ItemUpdateFlag)packet.ReadByte();
